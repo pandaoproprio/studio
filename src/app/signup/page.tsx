5d2e4b1e0db2a9f6222a890f38ee102e58296011
Juan Pablo Pinheiro <juanpablorj@gmail.com>
2025-07-16 13:13:22 +0000

    Dê mais granularidade, torne o sistema mais poderoso, inovação e robuste

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useRouter } from "next/navigation";
-import { Leaf, UserPlus } from "lucide-react";
+import { UserPlus } from "lucide-react";
 import Link from "next/link";
 
 export default function SignupPage() {
