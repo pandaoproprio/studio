8d5a04403a21ae6e3bd828b13783a06acce5b12e
Juan Pablo Pinheiro <juanpablorj@gmail.com>
2025-07-16 13:26:34 +0000

    Dê mais granularidade, torne o sistema mais poderoso, inovação e robuste

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { CheckCircle, ExternalLink } from "lucide-react";
 
