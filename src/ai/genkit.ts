24764945b0303e997c7d1fe90b62c142f733d7f4
Juan Pablo Pinheiro <juanpablorj@gmail.com>
2025-07-16 14:36:58 +0000

    continua o erro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {config} from 'dotenv';
-
-config();
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GOOGLE_API_KEY})],
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
 });
