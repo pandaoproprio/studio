cec918b129020a69fa4ab60e2c0ed45a53a01127
Juan Pablo Pinheiro <juanpablorj@gmail.com>
2025-10-15 16:11:05 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,16 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBnNpg3SGf-lasm1dzX0NH8nSG1ylehmUc",
+  authDomain: "studio-2770845557-a7da9.firebaseapp.com",
+  projectId: "studio-2770845557-a7da9",
+  storageBucket: "studio-2770845557-a7da9.appspot.com",
+  messagingSenderId: "532352991807",
+  appId: "1:532352991807:web:0b753e695502807370ae8a"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
