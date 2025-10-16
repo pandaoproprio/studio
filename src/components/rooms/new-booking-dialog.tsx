8d9831498cdf33fae8bf34fb839bde5bea9b3771
Juan Pablo Pinheiro <juanpablorj@gmail.com>
2025-10-16 05:11:17 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/components/rooms/new-booking-dialog.tsx
+++ src/components/rooms/new-booking-dialog.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
